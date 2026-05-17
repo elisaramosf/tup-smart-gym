5c21516b55e34e72e06bc9b7bb4ef9cbe82e0652
noder <noderdurand12@gmail.com>
2026-05-17 00:08:28 -0300

    Tp4-users

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsersComponent } from './user/user';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,UsersComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
