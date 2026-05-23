57dac426e721aa4001a48a798905fb7f613a735d
noder <noderdurand12@gmail.com>
2026-05-23 17:19:02 -0300

    A route was added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'users', component: UsersComponent}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UsersComponent } from './user/user';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UsersComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
