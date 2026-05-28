c304469b3b060f2b912566b2bd020d5e29fc44ba
Elisa Ramos Farinho <elisaramosfarinho@Elisas-MacBook-Pro.local>
2026-05-28 19:52:20 -0300

    saving main-layout changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { SettingsComponent } from './components/settings/settings'; 
+import { MainLayout } from './main-layout/main-layout';
+import { ItemsComponent } from './components/items/items';
+
 
 export const routes: Routes = [
-    { path: 'configuracion', component: SettingsComponent }
+    { 
+    path: '', 
+    component: MainLayout,
+    children: [
+      { path: 'items', component: ItemsComponent },
+      { path: 'configuracion', component: SettingsComponent },
+      { path: '', redirectTo: 'items', pathMatch: 'full' } 
+    ]
+  }
 ];
